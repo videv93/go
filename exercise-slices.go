ae2f2e3e0174183fba5974ea956a62e79a3347f5
videv93 <videv93@gmail.com>
2023-04-10 17:14:53 +0700

    feat: add go wiki

--- exercise-slices.go
+++ exercise-slices.go
@@ -1,8 +1,21 @@
 package main
 
-import "goloang.org/x/tour/pic"
+import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
+  // Create a slice of slices of uint8
+  pic := make([][]uint8, dy)
+  // Loop through the slice of slices
+  for y := range pic {
+    // Create a slice of uint8
+    pic[y] = make([]uint8, dx)
+    // Loop through the slice of uint8
+    for x := range pic[y] {
+      // Set the value of the slice of uint8
+      pic[y][x] = uint8((x + y) / 2)
+    }
+  }
+  return pic
 
 }
 
